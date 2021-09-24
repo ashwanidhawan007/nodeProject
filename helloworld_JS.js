8032571345d00d6cfcd70cc7b8238b9ea2b7e406
ashwanidhawan007 <ashwanikdhawan@gmail.com>
2021-09-24 16:58:08 +0400

    JS level2

--- helloworld_JS.js
+++ helloworld_JS.js
@@ -1,10 +1,11 @@
 //Create nodeJS server
 const http =  require("http");
-http.createServer((req,res)=>{
-    res.writeHead( 200,{'Content-Type':'tex/plain'})
-    res.end("hello world from nodeJS");
-}).listen(8080,'localhost');
-console.log("Server is listening at port 8080");
+const {stringify}  =  require("querystring");
 
-
-
+const serverCB  =   (req,res) => {
+    res.writeHead( 200,{'Content-Type':'tex/plain'});
+        res.end("hello world from nodeJS");
+};   
+http.createServer(serverCB).listen(8080,'localhost');
+console.log("Server is listening at port: 8080");
+console.log(serverCB);
