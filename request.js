41407c9a505fb70001305fb46f6eb1a776613ccb
ashwanidhawan007 <ashwanikdhawan@gmail.com>
2021-09-25 17:36:09 +0400

    request

---
+++ request.js
@@ -0,0 +1,13 @@
+const request = require("request");
+
+request('http://www.google.com', (error ,response, body) =>{
+if (error){
+console.log(error);
+}
+if (response){
+    console.log(response.statusCode);
+    }
+if (body){
+        console.log(body);
+    }
+});
