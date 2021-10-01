25acc1226937aa1a7a56ff4301fb5b9fc47629c0
preetidhawan <ashwanikdhawan@gmail.com>
2021-10-01 18:43:24 +0400

    .

--- url.js
+++ url.js
@@ -8,3 +8,4 @@ console.log(q.search); //returns '?year=2017&month=february'
 
 var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
 console.log(qdata.month); //returns 'february'
+
