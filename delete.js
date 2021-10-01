5a2bf110a1bd01f225aad63d9605138d24d4faa8
preetidhawan <ashwanikdhawan@gmail.com>
2021-10-01 20:14:23 +0400

    .

---
+++ delete.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+/*
+fs.unlink("./data.txt" , (err,data) => {
+    if (err) {
+        console.err(err) ;
+    }
+    else
+    {
+    console.log("file delete");
+    }   
+});
+*/
