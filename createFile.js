499abdbde0948e883aa94a2471b9670f1dd7d6c3
preetidhawan <ashwanikdhawan@gmail.com>
2021-10-01 18:55:10 +0400

    file

--- createFile.js
+++ createFile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 fs.writeFile("./data.txt","This is from async prog", (err) => {
     if (err){
         console.log(err);
-        console.log("File Write completed now ");
     }
-} );
-console.log("File Write completed ");
+        console.log("File Write completed now ")
+    } );
+console.log("File Write Processing");
