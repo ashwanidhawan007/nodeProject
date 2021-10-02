533520ed8578c0a52f3f5a72de1192ddcaed0029
preetidhawan <ashwanikdhawan@gmail.com>
2021-10-02 11:47:50 +0400

    .

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const router = express.Router();
+
+
 const app = express();
 
-app.use('/', (req, res) =>{
-    res.status(200).send("<h1> Hello world !!! </h1>");
+router.get('/Record', (req, res) =>{
+    res.status(200).send("<h1> This is from  Express routers !!! </h1>");
+    res.end();
 })
+
+app.use('/', (req, res) =>{
+    res.status(200).send("<h1> Hello world from Express !!! </h1>");
+})
+
+
 app.listen("8050" );
 console.log("Express is liestening 8050");
